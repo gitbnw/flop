86af13e6d944248154d5155897f2d97cbe2299f7
gitbnw <byron.weiss@gmail.com>
2016-05-22 13:32:49 -0500

    placesapi class

---
+++ app/helpers/google.rb
@@ -0,0 +1,10 @@
+require 'httparty'
+require 'json'
+
+module Gplaces
+
+GOOGLE_API_KEY = ENV['GOOGLE_API_KEY']
+
+
+
+end
---
+++ app/helpers/gplaces.rb
@@ -0,0 +1,16 @@
+require 'httparty'
+
+class PlacesAPI
+  include HTTParty
+  base_uri "maps.googleapis.com/maps/api/place/"
+  default_params :apiKey => ENV['GOOGLE_API_KEY']
+
+  def self.get_nearby_places(lng, lat, radius, types, name)
+    get("nearbysearch/json?location=#{lng},#{lat}&radius=#{radius}%types=#{types}&name=#{name}&key=#{:apiKey}")
+  end
+
+  def self.get_place_details(placeid)
+    get("details/json?placeid=#{placeid}&key=#{:apiKey}")
+  end
+
+end
