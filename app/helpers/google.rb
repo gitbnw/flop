bf8b15c06e422aefe53c42fb4b140cbd510b76a4
gitbnw <byron.weiss@gmail.com>
2016-05-23 14:06:51 +0000

    fix get_place_details method

--- app/helpers/google.rb
+++ app/helpers/google.rb
@@ -6,15 +6,14 @@ API_KEY = ENV['GOOGLE_API_KEY']
     include HTTParty
     base_uri "https://maps.googleapis.com"
 
-
     def self.get_nearby_places(lng, lat, radius, api_key = API_KEY)
       #https://maps.googleapis.com/maps/api/place/nearbysearch/output?parameters
       puts api_key
       get("/maps/api/place/nearbysearch/json?location=#{lng},#{lat}&radius=#{radius}&key=#{api_key}")
     end
 
-    def self.get_place_details(placeid)
-      get("/maps/api/place/details/json?placeid=#{placeid}&key=#{@api_key}")
+    def self.get_place_details(placeid, api_key = API_KEY)
+      get("/maps/api/place/details/json?placeid=#{placeid}&key=#{api_key}")
     end
 
   end
